2c4d3a22097f6bd9cc6cac52b1252606ae701448
DRohan007 <drohan940@gmail.com>
2019-09-20 09:25:00 +0100

    Sanders : Drop CNS and Location[TEMP]
    Signed-off-by: sai4041412 <sai404142@gmail.com>

--- sanders-vendor.mk
+++ sanders-vendor.mk
@@ -1309,28 +1309,23 @@ PRODUCT_PACKAGES += \
     libqct_resampler \
     TimeService \
     embms \
-    com.qti.location.sdk \
     com.qti.snapdragon.sdk.display \
     embmslibrary \
     imscmlibrary \
     qcrilhook \
-    com.qualcomm.location \
     ims \
     imssettings \
     qcrilmsgtunnel \
     izat.xt.srv \
     QtiTelephonyServicelibrary \
     atfwd \
-    cneapiclient \
     com.qti.dpmframework \
     com.quicinc.cne \
     dpmapi \
     qti-vzw-ims-internal \
     rcsimssettings \
     tcmclient \
-    com.quicinc.cne.api-V1.0-java \
     QtiTelephonyService \
-    CNEService \
     DTVPlayer \
     DTVService \
     com.fingerprints.extension
