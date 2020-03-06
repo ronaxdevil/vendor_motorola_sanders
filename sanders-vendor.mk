1d96fcfd957d611648c11f1ef3038f0f19bd649b
Dybios <dibyajyoti.samal@gmail.com>
2020-03-06 23:16:56 +0100

    sanders: Add com.qualcomm.location apk from addison - build com.qti.location.sdk.jar - Mark com.qualcomm.location apk as PRESIGNED
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- sanders-vendor.mk
+++ sanders-vendor.mk
@@ -1465,6 +1465,8 @@ PRODUCT_PACKAGES += \
     libgpustats \
     TimeService \
     embms \
+    com.qti.location.sdk \
+    com.qualcomm.location \
     com.qti.snapdragon.sdk.display \
     embmslibrary \
     libtinycompress_vendor \
